de34623bd8842d49e3b7e519b0cc45cf3a9b43ac
masamune <kenjiii534@gmail.com>
2022-09-27 14:12:10 +0900

    コミット忘れ

--- nisa_simulator_nuxt/nuxt.config.js
+++ nisa_simulator_nuxt/nuxt.config.js
@@ -24,7 +24,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: '~/plugins/chart/chart.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -59,7 +59,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
